aaa6fa62506da874c8f0cce8db7c3ae438f5fd3a
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2023-11-21 20:15:10 -0500

    Problem with input system
    This will need to be resolved in the next commit. The issue originates from the code that is generated by the input action asset.

--- Assets/Scripts/Inputs.cs
+++ Assets/Scripts/Inputs.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class Inputs : MonoBehaviour
 {
-    public static PlayerActions _actions;
-    public static PlayerController _player;
+    //public static PlayerActions _actions;
+    public static PlayerControl _player;
 
-    public static void BindNewPlayer(PlayerController player)
+    public static void BindNewPlayer(PlayerControl player)
     {
         _player = player;
     }
 
-    public static void Init(PlayerController player)
+    public static void Init(PlayerControl player)
     {
-        _actions = new PlayerActions();
+        //_actions = new PlayerActions();
         BindNewPlayer(player);
 
-        _actions.Player.Move.performed += ctx => _player.Move(ctx.ReadValue<Vector2>());
+        //_actions.Player.Move.performed += ctx => _player.Move(ctx.ReadValue<Vector2>());
 
+        PlayMode();
+    }
+
+    public static void PlayMode()
+    {
+        //_actions.Player.Enable();
+        //_actions.UI.Disable();
     }
 
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControl : MonoBehaviour
 {
     private Vector2 _move;
 
+    [SerializeField] private float _speed;
+
     void Awake()
     {
-        
+        Inputs.Init(this);
     }
 
     void Update()
     {
-        
+        transform.Translate(Vector2.right * (_speed * Time.deltaTime * _move.x), Space.Self);
     }
 
     public void Move(Vector2 direction)
