1f92a76a1177a2f5028482ae6998f3c5be1de8b6
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2023-11-26 15:38:29 -0500

    Added more for enemy
    Player can jump on top of enemy to defeat them, and if enemy falls below level, they are destroyed as well.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private LayerMask playerLayerMask;
+
     Rigidbody2D rb2D;
 
     [SerializeField] private float speed;
     private bool movingLeft = true;
+    private bool headStomped = false;
 
-    void Start()
+    void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
@@ -24,13 +27,25 @@ public class EnemyController : MonoBehaviour
         {
             transform.Translate(Vector2.left * (speed * Time.deltaTime), Space.Self);
         }
+
+        headStomped = Physics2D.BoxCast(GetComponent<Collider2D>().bounds.max, GetComponent<Collider2D>().bounds.size, 0f, Vector2.up, 0.1f, playerLayerMask);
+
+        if (transform.position.y < -12)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && headStomped == true)
         {
-
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "Player" && headStomped == false)
+        {
+            Debug.Log("Damaged player");
+            movingLeft = !movingLeft;
         }
         else
         {
